93ec3fc195a73c56380a98f9faad7e8c40e883c6
stevit <stefanomanighetti07@gmail.com>
2025-06-28 16:30:15 +0200

    nuovi file

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-// Script JS collegato correttamente
-console.log("Cataloghi pronti al download!");
+fetch("cataloghi.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Errore nel caricamento dei cataloghi.");
+    }
+    return response.json();
+  })
+  .then((cataloghi) => {
+    const container = document.querySelector(".cataloghi");
+
+    cataloghi.forEach((cat) => {
+      const div = document.createElement("div");
+      div.className = "catalogo";
+      div.innerHTML = `
+        <h2>${cat.titolo}</h2>
+        <p>${cat.descrizione}</p>
+        <img src="${cat.immagine}" alt="${cat.titolo}">
+        <a href="${cat.link}" target="_blank">Scarica PDF</a>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch((error) => {
+    console.error("Errore:", error);
+  });
